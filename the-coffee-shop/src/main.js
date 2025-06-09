96d1e1df2cc9cef39084b14fb3e919913001bae6
miio93 <villegasg.jose93@gmail.com>
2025-06-09 11:38:59 -0700

    Update vite config and remove old main.js scss ref

--- the-coffee-shop/src/main.js
+++ the-coffee-shop/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
 import './assets/scss/main.scss'
 
 createApp(App).mount('#app')
--- the-coffee-shop/vite.config.js
+++ the-coffee-shop/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/the-coffee-shop/', //  IMPORTANT: Replace 'the-coffee-shop' with your GitHub repository name!
+  base: '/coffee-shop/',
 })
